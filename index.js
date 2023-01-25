7b89954c3371823559505809042531f8bc2e0f36
Andrew Dutson <Dutsandrew@gmail.com>
2023-01-24 19:48:06 -0700

    reworked node http serve, added css

--- index.js
+++ index.js
@@ -4,39 +4,54 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content, 'utf8');
-    });
-    return;
+  // build file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  // get extension of file
+  let extName = path.extname(filePath);
+
+  // initial content type
+  let contentType = 'text/html';
+
+  // check extension for content type
+  switch(extName) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
 
-  if (req.url === '/about') {
-    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+  // read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+        });
+      } else {
+        // server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // successful response
+      res.writeHead(200, { 'Content-Type': contentType });
       res.end(content, 'utf8');
-    });
-    return;
-  }
-
-  if (req.url === '/contact-me') {
-    fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content, 'utf8');
-    });
-    return;
-  }
-
-  fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-    if (err) throw err;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(content, 'utf8');
+    }
   });
-  return;
 
 });
 
@@ -47,3 +62,38 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// old code before reworking to a cleaner solution
+  // if (req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { 'Content-Type': 'text/html' });
+  //     res.end(content, 'utf8');
+  //   });
+  //   return;
+  // }
+
+  // if (req.url === '/about') {
+  //   fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { 'Content-Type': 'text/html' });
+  //     res.end(content, 'utf8');
+  //   });
+  //   return;
+  // }
+
+  // if (req.url === '/contact-me') {
+  //   fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { 'Content-Type': 'text/html' });
+  //     res.end(content, 'utf8');
+  //   });
+  //   return;
+  // }
+
+  // fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+  //   if (err) throw err;
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.end(content, 'utf8');
+  // });
+  // return;
